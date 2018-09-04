b7ae8a2f14178caefc1e664edeb8e65e4717db3f
Joe Peterson <joe.peterson@galvanize.com>
2018-09-04 13:34:43 -0600

    Fix failing test of VideoList

--- src/components/VideoList/VideoList.test.js
+++ src/components/VideoList/VideoList.test.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import VideoList from './VideoList';
-import Video from '../Video/Video';
-import { shallow } from 'enzyme';
-import sinon from 'sinon';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { VideoList } from './VideoList'
+import Video from '../Video/Video'
+import { shallow } from 'enzyme'
+import sinon from 'sinon'
 
 it('displays a list of video titles', () => {
-  const videoData = [{title: 'Star Wars'}, {title: 'Star Trek II'}];
-  const videoListWrapper = shallow(<VideoList videos={videoData} />);
-  const videos = videoListWrapper.find(Video);
-  expect(videos).toHaveLength(2);
+  const videoData = [{title: 'Star Wars'}, {title: 'Star Trek II'}]
+  const videoListWrapper = shallow(<VideoList videos={videoData} />)
+  const videos = videoListWrapper.find(Video)
+
+  expect(videos).toHaveLength(2)
 });
