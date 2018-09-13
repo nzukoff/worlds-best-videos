5a8975a59193ab83b9e0523aa461c958b130df66
Joe Peterson <joe.peterson@galvanize.com>
2018-09-12 20:00:14 -0600

    Add initial SpringBoot code

---
+++ src/main/java/com/galvanize/worldsbestvideos/WorldsBestVideosApplication.java
@@ -0,0 +1,12 @@
+package com.galvanize.worldsbestvideos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldsBestVideosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldsBestVideosApplication.class, args);
+	}
+}
