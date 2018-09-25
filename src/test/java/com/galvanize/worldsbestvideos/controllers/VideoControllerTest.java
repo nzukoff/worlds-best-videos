0e1586027a2e04732b8897b25565b7da1d3616ec
Joe Peterson <joe.peterson@galvanize.com>
2018-09-24 18:19:06 -0600

    Renamed VideoControllerTest.java

--- src/test/java/com/galvanize/worldsbestvideos/controllers/VideoControllerTest.java
+++ src/test/java/com/galvanize/worldsbestvideos/controllers/VideoControllerTest.java
@@ -22,7 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-public class WorldBestVideosControllerTest {
+public class VideoControllerTest {
     @Autowired
     MockMvc mvc;
 
